aaa68ba99bfc6dffe391909c40170352fa1bed7e
Jeremy (Hutch) Hutchcraft <jhutchcraft@partnercomm.net>
2018-02-09 14:35:46 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #333;
+  color: white;
+}
+* {
+  -webkit-font-smoothing: antialiased;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="stopwatch-wrapper" v-on:keypress.space="startTimer()">
+    <h1 class="seconds">{{ seconds }}s</h1>
+
+    <div class="stopwatch-controls">
+
+      <button class="btn btn--start" @click="startTimer()" v-on:keypress.space="startTimer()" :class="{'running': running}">Start</button>
+      <button class="btn btn--stop" @click="resetTimer()" v-on:keypress.space.prevent="startTimer()">Reset</button>
+      
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      elapsed: 0,
+      timer: false,
+      running: false,
+      start: Date.now(),
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    seconds() {
+      return (this.elapsed / 1000).toFixed(1);
+    }
+  },
+  methods: {
+    tick() {
+      this.elapsed = new Date() - this.start;
+    },
+    startTimer() {
+      let vm = this;
+      console.log('starting');
+      clearInterval(this.timer);
+      this.start = Date.now();
+      this.timer = setInterval(vm.tick, 50);
+      this.running = true;
+    },
+    resetTimer() {
+      console.log('resetting');
+      this.elapsed = 0;
+      clearInterval(this.timer);
+      this.running = false;
+    }
+  },
+  watch: {
+    elapsed(value){
+    }
+  },
+  created() {
+    let vm = this;
+    document.body.onkeyup = function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      if(e.keyCode == 32){
+          if(vm.running){
+            vm.resetTimer();
+          } else {
+            vm.startTimer();
+          }
+      }
+    }
+  },
+  destroyed() {
+    clearInterval(this.timer);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+$primary-color: #b5985a;
+$border-color: darken($primary-color, 10%);
+
+* {
+  color: white;
+}
+
+h1, h2 {
+  font-weight: normal;
+  margin-top: 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: $primary-color;
+}
+
+.seconds {
+  font-size: 4rem;
+  font-weight: bold;
+  padding: 2rem 5rem;
+  display: inline-block;
+  border: 1px solid $primary-color;
+}
+
+.stopwatch-controls {
+  .btn {
+    cursor: pointer;
+    appearance: none;
+    border: none;
+    background-color: $primary-color;
+    color: white;
+    font-size: 1.4rem;
+    padding: 1em 3em;
+    border-radius: 4px;
+    overflow: hidden;
+    border-top: 2px solid lighten($primary-color, 20%);
+    border-right: 1px solid lighten($primary-color, 20%);
+    border-left: 1px solid $border-color;
+    border-bottom: .5rem solid $border-color;
+    transform: translate3d(0,-10px,0);
+    transition: all 0.25s ease-out;
+    will-change: transform;
+    font-weight: bold;
+    &:focus,
+    &:hover {
+      outline: none;
+      background-color: lighten($primary-color, 5%);
+    }
+    &.running {
+      transform: translate3d(0,0px,0);
+      border-bottom: .05rem solid $border-color;
+    }
+  }
+}
+
+</style>
